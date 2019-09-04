65f76a6e27d8d1616ce936d5df6c47de94517fd7
Jorik Schellekens <joriks@matrix.org>
2019-09-04 15:41:42 +0100

    Setup peer with persistant identity.

--- go.mod
+++ go.mod
@@ -2,11 +2,4 @@ module github.com/matrix-org/libp2p-proxy
 
 go 1.12
 
-require (
-	golang.org/x/crypto v0.0.0-20190829043050-9756ffdc2472 // indirect
-	golang.org/x/lint v0.0.0-20190409202823-959b441ac422 // indirect
-	golang.org/x/net v0.0.0-20190827160401-ba9fcec4b297 // indirect
-	golang.org/x/sys v0.0.0-20190902133755-9109b7679e13 // indirect
-	golang.org/x/text v0.3.2 // indirect
-	golang.org/x/tools v0.0.0-20190903163617-be0da057c5e3 // indirect
-)
+require github.com/libp2p/go-libp2p v0.3.1 // indirect
--- main.go
+++ main.go
@@ -18,16 +18,104 @@
 package main
 
 import (
+    "context"
+    "crypto/rand"
     "flag"
-    "fmt"
+    "io/ioutil"
+    "log"
 
-//    "github.com/libp2p/go-libp2p"
+    "github.com/libp2p/go-libp2p"
+    "github.com/libp2p/go-libp2p-core/crypto"
 )
 
-func main() {
-    httpTarget := flag.String("http-target", "http://127.0.0.1:8008", "The HTTP host+port the requests to this peer are sent to")
-    httpPort := flag.String("http-port", "8888", "The HTTP port to listen on")
-    identityFile := flag.String("identity-file", "./identity", "A file containig the libp2p peer's private key.")
+var (
+    httpTarget   = flag.String("http-target", "http://127.0.0.1:8008", "The HTTP host+port the requests to this peer are sent to")
+    httpPort     = flag.String("http-port", "8888", "The HTTP port to listen on")
+    identityFile = flag.String("identity-file", "./identity", "A file containig the libp2p peer's private key.")
+)
 
-    fmt.Printf("%s %s %s", *httpTarget, *httpPort, *identityFile)
+func init() {
+    flag.Parse()
 }
+
+func main() {
+
+    ctx, cancel := context.WithCancel(context.Background())
+    defer cancel()
+
+    privKey := getLibp2pPrivKey(*identityFile)
+
+    h, err := libp2p.New(
+	ctx,
+	libp2p.Identity(privKey),
+    )
+
+    if err != nil {
+	log.Fatal("Couldn't start libp2p host.")
+    }
+
+    log.Printf("Started host with id %s", h.ID())
+
+}
+
+// Retrieve the identity (the private key) for the p2p peer
+// Tries to retrieve the identity from the specified file.
+// If the file doesn't exist it will try to create it.
+// If the content of the file is not a valid encoded p2p
+// PrivKey it will exit.
+func getLibp2pPrivKey(identityFile string) crypto.PrivKey {
+    encodedPrivateKey, err := ioutil.ReadFile(identityFile)
+
+    var privKey crypto.PrivKey
+
+    if err != nil {
+	log.Printf("Couldn't find an identity in %s, creating new idenitity.\n", identityFile)
+	privKey = createPeerIdentity(identityFile)
+	log.Println("Identity created")
+    } else {
+	handler := func(err error) {
+	    if err != nil {
+		log.Fatalf("The identity in %s is invalid. Shutting down proxy.", identityFile)
+	    }
+	}
+
+	decodedPrivateKey, err := crypto.ConfigDecodeKey(string(encodedPrivateKey))
+
+	handler(err)
+
+	privKey, err = crypto.UnmarshalPrivateKey(decodedPrivateKey)
+
+	handler(err)
+    }
+
+    return privKey
+}
+
+func createPeerIdentity(identityFile string) crypto.PrivKey {
+    r := rand.Reader
+
+    privKey, _, err := crypto.GenerateKeyPairWithReader(crypto.RSA, 2048, r)
+
+    if err != nil {
+	panic(err)
+    }
+
+    privKeyBytes, err := crypto.MarshalPrivateKey(privKey)
+
+    if err != nil {
+	panic(err)
+    }
+
+    encodedPrivateKey := crypto.ConfigEncodeKey(privKeyBytes)
+
+    // Only the current user has access to the identity file
+    err = ioutil.WriteFile(identityFile, []byte(encodedPrivateKey), 0600)
+
+    if err != nil {
+	log.Fatalf("Couldn't write the idenity file to %s", identityFile)
+    }
+
+    return privKey
+}
+
+
